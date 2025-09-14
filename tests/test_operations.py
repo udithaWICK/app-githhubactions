8da7e3b9159d218ca53fb2ffbad7a8a82f90f7cf
admin <udithac@gmail.com>
2025-09-14 16:00:18 +0200

    Unit test cases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,15 @@
+from src.math_opertions import add, sub
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+    assert add(-1, 1) == 0
+    assert add(-1, 1) == -2
+
+
+def test_Sub():
+    assert sub(5, 3) == 2
+    assert sub(0, 2) == -2
+    assert sub(2, 0) == 0
+    assert sub(9, 7) == 5
