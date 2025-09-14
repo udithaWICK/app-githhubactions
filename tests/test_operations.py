a7aee500a3b9bb62287cec7bef4653b4364f9897
admin <udithac@gmail.com>
2025-09-14 17:35:58 +0200

    Modify the test operations mistak

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,11 +5,11 @@ def test_add():
     assert add(2, 3) == 5
     assert add(0, 0) == 0
     assert add(-1, 1) == 0
-    assert add(-1, 1) == -2
+    assert add(-1, 1) == 0
 
 
 def test_Sub():
     assert sub(5, 3) == 2
     assert sub(0, 2) == -2
     assert sub(2, 0) == 0
-    assert sub(9, 7) == 5
+    assert sub(9, 7) == 2
